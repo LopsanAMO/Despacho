9471c51c38e2e0d8226cb3353d528563751d8492
LopsanAMO <makiki789@gmail.com>
2018-03-14 01:24:02 -0600

    adding document app admin site

--- apps/documents/admin.py
+++ apps/documents/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import (
+    UserClient, FolderClient, UserClientModification, Document,
+    DocumentModification
+)
 
-# Register your models here.
+@admin.register(UserClient)
+class UserClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created')
+    list_filter = ('created',)
+
+
+@admin.register(FolderClient)
+class FolderClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'created')
+    list_filter = ('user', 'created')
+
+
+@admin.register(UserClientModification)
+class UserClientModificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'user_folder', 'updated')
+    list_filter = ('user', 'user_folder', 'updated')
+
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'folder', 'created')
+    list_filter = ('folder', 'created')
+
+
+@admin.register(DocumentModification)
+class DocumentModificationAdmin(admin.ModelAdmin):
+    list_display = ('document', 'user', 'updated')
+    list_filter = ('document', 'user', 'updated')

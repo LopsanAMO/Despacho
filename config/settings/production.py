79a49e6ea1d3775fcad538659091b96b332c5b03
Travis <lopsan.molina@gmail.com>
2018-03-28 08:39:17 +0000

    PEP-8 Fix

--- config/settings/production.py
+++ config/settings/production.py
@@ -3,12 +3,12 @@ from .common import *
 DEBUG = False
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': '',
-            'USER': '',
-            'PASSWORD': '',
-            'HOST': '',
-            'PORT': '',
-        }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
 }

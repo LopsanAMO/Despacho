5c7e7fd5da7ab96236b9860acc3ec2c6530daae9
LopsanAMO <lopsan.molina@gmail.com>
2018-03-28 02:37:01 -0600

    adding production config

---
+++ config/settings/production.py
@@ -0,0 +1,14 @@
+from .common import *
+
+DEBUG = False
+
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': '',
+            'USER': '',
+            'PASSWORD': '',
+            'HOST': '',
+            'PORT': '',
+        }
+}

c849d9eb7a530e1b371b357847bf7befb382d556
LopsanAMO <lopsan.molina@gmail.com>
2018-04-02 06:09:21 -0500

    new pagination limit

--- utils/helpers.py
+++ utils/helpers.py
@@ -71,6 +71,6 @@ class ErrorMesages(object):
 
 
 class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 9
     page_size_query_param = 'limit'
     max_page_size = 20

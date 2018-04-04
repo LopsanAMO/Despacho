ab5a2e52f78f1b786db7ac880434153d7933bd71
LopsanAMO <lopsan.molina@gmail.com>
2018-04-03 21:26:40 -0500

    .

---
+++ config/settings/heroku.py
@@ -0,0 +1,19 @@
+from .common import *
+
+DEBUG = False
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
+
+STATIC_URL = 'https://despacho-cloud.s3-us-west-2.amazonaws.com/'
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_STORAGE_BUCKET_NAME = 'despacho-cloud'
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}

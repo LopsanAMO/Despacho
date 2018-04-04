f1f23588622e67b60731217530e9b3004ade09cc
LopsanAMO <lopsan.molina@gmail.com>
2018-04-04 02:46:49 -0500

    .

--- config/wsgi.py
+++ config/wsgi.py
@@ -4,7 +4,7 @@ from django.core.wsgi import get_wsgi_application
 
 if os.getenv('SETTINGS', None) is not None:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", os.getenv('SETTINGS'))
-else
+else:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.develop")
 
 application = get_wsgi_application()

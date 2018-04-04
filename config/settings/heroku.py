15b41f4e5f5da5618a94555cebe5e6b894b568e2
LopsanAMO <lopsan.molina@gmail.com>
2018-04-03 22:59:18 -0500

    settings db

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -20,7 +20,11 @@ MEDIA_ROOT = MEDIA_URL
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DBNAME'),
+        'USER': os.getenv('DBUSER'),
+        'PASSWORD': os.getenv('DBPASSWORD'),
+        'HOST': , os.getenv('DBHOST'),
+        'PORT': '5432',
     }
 }

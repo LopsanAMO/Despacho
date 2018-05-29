ee1611fe7d70713d9dd8feca61ddebb4f12161e4
LopsanAMO <lopsan.molina@gmail.com>
2018-05-29 04:07:56 -0500

    added Log model

--- apps/documents/admin.py
+++ apps/documents/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from .models import (
-    UserClient, FolderClient, UserClientModification, Document,
-    DocumentModification
-)
+from .models import UserClient, FolderClient, Document, Log
 
 
 @admin.register(UserClient)
@@ -17,19 +14,13 @@ class FolderClientAdmin(admin.ModelAdmin):
     list_filter = ('user', 'created')
 
 
-@admin.register(UserClientModification)
-class UserClientModificationAdmin(admin.ModelAdmin):
-    list_display = ('user', 'user_folder', 'updated')
-    list_filter = ('user', 'user_folder', 'updated')
-
-
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ('name', 'folder', 'created')
     list_filter = ('folder', 'created')
 
 
-@admin.register(DocumentModification)
-class DocumentModificationAdmin(admin.ModelAdmin):
-    list_display = ('document', 'user', 'updated')
-    list_filter = ('document', 'user', 'updated')
+@admin.register(Log)
+class LogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action', 'updated')
+    list_filter = ('user', 'action', 'updated')
--- apps/documents/models.py
+++ apps/documents/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.template import defaultfilters
 from datetime import datetime
 from users.models import User
+from enum import Enum
 
 
 class UserClient(models.Model):
@@ -158,16 +159,35 @@ class Document(models.Model):
         verbose_name_plural = 'Documentos'
 
 
-class DocumentModification(models.Model):
-    document = models.ForeignKey(
-        Document,
-        verbose_name='Documento',
+class Log(models.Model):
+    class NOTIFICATION_TYPE(Enum):
+        create_client = ('create_client', 'Cliente Creado')
+        update_client = ('update_client', 'Cliente Actualizado')
+        delete_client = ('delete_client', 'Cliente Eliminado')
+        create_folder = ('create_folder', 'Folder Creado')
+        update_folder = ('update_folder', 'Folder Actualizado')
+        delete_folder = ('delete_folder', 'Folder Eliminado')
+        create_document = ('create_document', 'Documento, Creado')
+        update_document = ('update_document', 'Documento Actializado')
+        delete_document = ('delete_Document', 'Documento Eliminado')
+    
+        @classmethod
+        def get_value(cls, _type):
+            try:
+                return cls[_type].value[0]
+            except KeyError:
+                return ''
+
+    action = models.CharField(
+        verbose_name='Tipo de accion',
+        choices=[x.value for x in NOTIFICATION_TYPE],
+        null=False,
         blank=True,
-        on_delete=models.CASCADE
+        max_length=20
     )
     user = models.ForeignKey(
         User,
-        verbose_name='Usuario que modifico',
+        verbose_name='Usuario',
         blank=True,
         on_delete=models.CASCADE
     )
@@ -176,10 +196,16 @@ class DocumentModification(models.Model):
         blank=True,
         auto_now_add=True
     )
+    description = models.CharField(
+        verbose_name='Descripcion',
+        max_length=300,
+        blank=True,
+        null=True
+    )
 
     def __str__(self):
-        return "Documento modificado: {}".format(self.document.name)
+        return '{} - {}'.format(self.user.email, self.action)
 
     class Meta:
-        verbose_name = 'Documento Modificado'
-        verbose_name_plural = 'Documentos modificados'
+        verbose_name = 'Registro'
+        verbose_name_plural = 'Registros'
